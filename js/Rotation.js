88dd4ae077ac9486a8e7808e655cb2dfd8b5b862
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2020-12-26 10:01:37 +0530

    Update Rotation.js

--- js/Rotation.js
+++ js/Rotation.js
@@ -1,68 +1,52 @@
-
-AFRAME.registerComponent("driver-rotation-reader", {
-  init: function() {
-    console.log("Welcome to Virtual Scuba Diving !!!");
+//Diver rotation component
+AFRAME.registerComponent("diver-rotation-reader", {
+  schema: {
+    speedOfRoation: { type: "number", default: 0 },
+    speedOfAscent: { type: "number", default: 0 }
   },
-  tick: function() {
-    this.setVelocity(this.el.body);
-  },
-  update: function() {
-    // Key Down Event
-    window.addEventListener("keydown", e => {
-      const driverElementRotation = this.el.getAttribute("rotation");
-      console.log(this.el.body);
-      const body = this.el.body;
-      const driverRotation = body.angularVelocity;
-      const driverPosition = body.position;
+  init: function () {
+    window.addEventListener("keydown", (e) => {
 
+      //get the data from the attributes
+      this.data.speedOfRoation = this.el.getAttribute("rotation");      
+      this.data.speedOfAscent = this.el.getAttribute("position");
+
+      var diverRotation = this.data.speedOfRoation;      
+      var diverPosition = this.data.speedOfAscent;
+
+      //control the attributes with the Arrow Keys
       if (e.key === "ArrowRight") {
-        if (driverElementRotation.x >= -35 && driverElementRotation.x <= 30) {
-          body.angularVelocity.set(0, 0, -0.2);
-        } else {
-          body.angularVelocity.set(0, 0, 0);
+        if (diverRotation.x < 10) {
+          diverRotation.x += 0.5;
+          this.el.setAttribute("rotation", diverRotation);
         }
       }
       if (e.key === "ArrowLeft") {
-        if (driverElementRotation.x <= 35 && driverElementRotation.x >= -30) {
-          body.angularVelocity.set(0, 0, 0.2);
-        } else {
-          body.angularVelocity.set(0, 0, 0);
+        if (diverRotation.x > -10) {
+          diverRotation.x -= 0.5;
+          this.el.setAttribute("rotation", diverRotation);
         }
       }
       if (e.key === "ArrowUp") {
-        if (driverElementRotation.z > -50 && driverElementRotation.z <= -10) {
-          body.angularVelocity.set(0.2, 0, 0);
-        } else {
-          body.angularVelocity.set(0, 0, 0);
+        if (diverRotation.z < 20) {
+          diverRotation.z += 0.5;
+          this.el.setAttribute("rotation", diverRotation);
+        }
+        if (diverPosition.y < 2) {
+          diverPosition.y += 0.01;
+          this.el.setAttribute("position", diverPosition);
         }
       }
       if (e.key === "ArrowDown") {
-        if (driverElementRotation.z <= -10 && driverElementRotation.z >= -50) {
-          body.angularVelocity.set(-0.2, 0, 0);
-        } else {
-          body.angularVelocity.set(0, 0, 0);
+        if (diverRotation.z > -10) {
+          diverRotation.z -= 0.5;
+          this.el.setAttribute("rotation", diverRotation);
+        }
+        if (diverPosition.y > -2) {
+          diverPosition.y -= 0.01;
+          this.el.setAttribute("position", diverPosition);
         }
       }
     });
-
-    // Key Up Event
-    window.addEventListener("keyup", e => {
-      const body = this.el.body;
-      if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
-        body.angularVelocity.set(0, 0, 0);
-      }
-      if (e.key === "ArrowUp" || e.key === "ArrowDown") {
-        body.angularVelocity.set(0, 0, 0);
-      }
-    });
-  },
-  setVelocity: function(body) {
-    if (body !== undefined) {
-      // Set velocity to 0.1 to float in the air
-      body.velocity.set(0.1, 0.1, 0.1);
-      // Initial position of driver
-      const initPosition = body.initPosition;
-      body.position.set(initPosition.x, initPosition.y, initPosition.z);
-    }
   }
 });
